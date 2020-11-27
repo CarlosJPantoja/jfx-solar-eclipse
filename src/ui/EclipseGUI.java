e1c854a7b0e81cecd261a9c2c19d5344a5a80570
CarlosJPantoja <61593197+CarlosJPantoja@users.noreply.github.com>
2020-11-27 15:01:20 -0500

    functional eclipse

---
+++ src/ui/EclipseGUI.java
@@ -0,0 +1,43 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import thread.BallThread;
+
+public class EclipseGUI {
+	
+	@FXML
+	private Pane pane;
+
+    @FXML
+    private Circle sun;
+
+    @FXML
+    private Circle moon;
+    
+    @FXML
+    private Slider bar;
+
+    BallThread bt;
+    
+    @FXML
+    public void moveMoon(ActionEvent event) {
+    	bt.isMoving(false);
+    	bt = new BallThread(moon, pane, bar, sun);
+    	bt.isMoving(true);
+    	bt.start();
+    }
+
+    @FXML
+    public void stopMoon(ActionEvent event) {
+    	bt.isMoving(false);
+    }
+    
+    public void initialize() {
+    	bt = new BallThread(moon, pane, bar, sun);
+    }
+
+}
